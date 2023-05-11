d938f5d610dbcf5d3878156a89dbaa031a2b3174
Caleb Price <calebsp12@gmail.com>
2023-05-11 11:02:18 -0600

    test

--- UnitTest/names.py
+++ UnitTest/names.py
@@ -15,7 +15,7 @@ def get_first_last(name):
     """
     return tuple(name.split(" "))
 
-
+# add comment
 class Testing(unittest.TestCase):
     def test_names(self):
         self.assertEqual(get_first_last("Caleb Price"), ("Caleb", "Price"))
